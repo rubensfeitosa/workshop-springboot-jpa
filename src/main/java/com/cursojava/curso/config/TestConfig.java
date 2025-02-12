bfb6b27fbc5e6848d08cc7aab412d616ee8ed369
Rubens Feitosa <rubens.feitosa3@gmail.com>
2025-02-12 15:34:33 -0300

    Criando pagamento

--- src/main/java/com/cursojava/curso/config/TestConfig.java
+++ src/main/java/com/cursojava/curso/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cursojava.curso.entities.Category;
 import com.cursojava.curso.entities.Order;
 import com.cursojava.curso.entities.OrderItem;
+import com.cursojava.curso.entities.Payment;
 import com.cursojava.curso.entities.Product;
 import com.cursojava.curso.entities.User;
 import com.cursojava.curso.entities.enums.OrderStatus;
@@ -81,7 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
 
+		orderRepository.save(o1);
 		
 	}
 	
