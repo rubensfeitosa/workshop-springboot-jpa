c0dba59a7fb86dad4ec9f78ca5d5c17a8655018a
Rubens Feitosa <rubens.feitosa3@gmail.com>
2025-02-12 16:02:11 -0300

    inserindo um use pelo postman

--- src/main/java/com/cursojava/curso/services/UserService.java
+++ src/main/java/com/cursojava/curso/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.cursojava.curso.entities.User;
 import com.cursojava.curso.repositories.UserRepository;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -24,4 +27,34 @@ public class UserService {
 		//return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+//	public void delete(Long id) {
+//		try {
+//			repository.deleteById(id);
+//		} catch (EmptyResultDataAccessException e) {
+//			throw new ResourceNotFoundException(id);
+//		} catch (DataIntegrityViolationException e) {
+//			throw new DatabaseException(e.getMessage());
+//		}
+//	}
+//	
+//	public User update(Long id, User obj) {
+//		try {
+//			User entity = repository.getReferenceById(id);
+//			updateData(entity, obj);
+//			return repository.save(entity);
+//		} catch (EntityNotFoundException e) {
+//			throw new ResourceNotFoundException(id);
+//		}	
+//	}
+//
+//	private void updateData(User entity, User obj) {
+//		entity.setName(obj.getName());
+//		entity.setEmail(obj.getEmail());
+//		entity.setPhone(obj.getPhone());
+//	}
 }
